ce8b8288138ad24eed8b583b717274b0473a0000
Eron Silva <eron.silva@digivox.com.br>
2021-03-20 23:10:13 -0300

    Adicionado arquivos de aspas

---
+++ tipo_de_aspas.js
@@ -0,0 +1,20 @@
+console.log("Bem vindo ao starter")
+
+
+/*
+ 
+""// aspas duplas 
+''// aspas simples
+``// template literals ou templates literais
+
+*/
+
+
+//Diferença de utilizar a crase é que você consegue utilizar em multiplas linhas ou interpolação(Colocar expressões dentro da string)
+
+
+console.log(`Olá ${1 + 1}
+linha 1
+linha 2
+linha 3
+`)
